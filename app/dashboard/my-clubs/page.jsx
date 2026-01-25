6911eceb108545b576d144670fad549426968c75
PawanKumar-ops <pawan01200kumar@gmail.com>
2026-01-25 10:48:00 +0530

    My Clus frontend Completed

--- app/dashboard/my-clubs/page.jsx
+++ app/dashboard/my-clubs/page.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 import "./my-clubs.css"
 
 const MyClubsPage = () => {
+
+  
+
   return (
       <div className='my-clubsbody'>
         <div className="clubhead">
@@ -20,8 +23,102 @@ const MyClubsPage = () => {
 
         <div className="clublist">
           <div className="clubinfo">
-            
+            <div className="clubppic">
+              <img src="/Defaultclublogo.svg" alt="" />
+            </div>
+            <div className="clubpinfo">
+              <h1>Innovation Cell</h1>
+              <div>
+                <img src="/dashboard/MyClubs.svg" alt="" />
+                <p>295</p>
+              </div>
+            </div>
+            <div className="userposcont">
+            <div className="userpos">Member</div>
+            </div>
+
           </div>
+          <div className="clubinfo">
+            <div className="clubppic">
+              <img src="/Defaultclublogo.svg" alt="" />
+            </div>
+            <div className="clubpinfo">
+              <h1>Innovation Cell</h1>
+              <div>
+                <img src="/dashboard/MyClubs.svg" alt="" />
+                <p>295</p>
+              </div>
+            </div>
+            <div className="userposcont">
+            <div className="userpos">Member</div>
+            </div>
+
+          </div>
+          <div className="clubinfo">
+            <div className="clubppic">
+              <img src="/Defaultclublogo.svg" alt="" />
+            </div>
+            <div className="clubpinfo">
+              <h1>Innovation Cell</h1>
+              <div>
+                <img src="/dashboard/MyClubs.svg" alt="" />
+                <p>295</p>
+              </div>
+            </div>
+            <div className="userposcont">
+            <div className="userpos">Member</div>
+            </div>
+
+          </div>
+          <div className="clubinfo">
+            <div className="clubppic">
+              <img src="/Defaultclublogo.svg" alt="" />
+            </div>
+            <div className="clubpinfo">
+              <h1>Innovation Cell</h1>
+              <div>
+                <img src="/dashboard/MyClubs.svg" alt="" />
+                <p>295</p>
+              </div>
+            </div>
+            <div className="userposcont">
+            <div className="userpos">Member</div>
+            </div>
+
+          </div>
+          <div className="clubinfo">
+            <div className="clubppic">
+              <img src="/Defaultclublogo.svg" alt="" />
+            </div>
+            <div className="clubpinfo">
+              <h1>Innovation Cell</h1>
+              <div>
+                <img src="/dashboard/MyClubs.svg" alt="" />
+                <p>295</p>
+              </div>
+            </div>
+            <div className="userposcont">
+            <div className="userpos">Member</div>
+            </div>
+
+          </div>
+          <div className="clubinfo">
+            <div className="clubppic">
+              <img src="/Defaultclublogo.svg" alt="" />
+            </div>
+            <div className="clubpinfo">
+              <h1>Innovation Cell</h1>
+              <div>
+                <img src="/dashboard/MyClubs.svg" alt="" />
+                <p>295</p>
+              </div>
+            </div>
+            <div className="userposcont">
+            <div className="userpos">Member</div>
+            </div>
+
+          </div>
+          
         </div>
 
 
