c54c158e7e2521fca30eb021be6ee6b5d1e2243b
Anujsharma7027 <anujsharma9466@gmail.com>
2026-01-29 16:55:41 +0530

    Google Signin added

--- components/Logincard.jsx
+++ components/Logincard.jsx
@@ -75,7 +75,7 @@ const Loginbtn = () => {
           <div className="subtitle">Sign in to continue</div>
 
           {/* Google Button */}
-          <button className="social-btn">
+          <button className="social-btn"  onClick={() => { signIn("google") }} >
             <svg width="20" height="20" viewBox="0 0 48 48">
               <path fill="#FFC107"
                 d="M43.6 20.5H42V20H24v8h11.3C33.7 32.7 29.3 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.9 1.2 8 3.1l5.7-5.7C34.2 6.1 29.4 4 24 4 12.9 4 4 12.9 4 24s8.9 20 20 20 20-8.9 20-20c0-1.3-.1-2.2-.4-3.5z" />
@@ -159,7 +159,7 @@ const Loginbtn = () => {
           <div className="subtitle">Join us today and get started</div>
 
           {/* Google Button */}
-          <button className="social-btn">
+          <button className="social-btn" onClick={() => signIn("google", { callbackUrl: "/dashboard" })} >
             <svg width="20" height="20" viewBox="0 0 48 48">
               <path fill="#FFC107"
                 d="M43.6 20.5H42V20H24v8h11.3C33.7 32.7 29.3 36 24 36c-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.9 1.2 8 3.1l5.7-5.7C34.2 6.1 29.4 4 24 4 12.9 4 4 12.9 4 24s8.9 20 20 20 20-8.9 20-20c0-1.3-.1-2.2-.4-3.5z" />
